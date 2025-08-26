d30f5c05a66bd49b263a966915bb4dcd45084a03
hk12589 <hk12589@gmail.com>
2025-08-26 20:19:58 +0530

    Add files via upload

--- pages/3_✍️_blogs.py
+++ pages/3_✍️_blogs.py
@@ -19,49 +19,49 @@ BLOG_POSTS = [
     {
         "title": "Yoga and Ayurveda: A Perfect Synergy 🧘‍♀️🌿",
         "description": "Welcome to our comprehensive guide on the beautiful synergy between Yoga and Ayurveda...",
-        "image_path": "img/models/1.jpg",
+        "image_path": "img/1.jpg",
         "link": "Yoga_and_Ayurveda_blog_page"
     },
     {
         "title": "Top 10 Ayurvedic Herbs for Daily Health 🌿✨",
         "description": "Welcome to our comprehensive guide on the Top 10 Ayurvedic Herbs for your daily wellness routine...",
-        "image_path": "img/models/2.jpg",
+        "image_path": "img/2.jpg",
         "link": "Top_10_Ayurvedic_Herbs_Blog_Page" # Placeholder link for other blogs
     },
     {
         "title": "Ayurvedic Diet: Eating According to Your Dosha 🍽️�",
         "description": "Welcome to our comprehensive guide on the Ayurvedic Diet, where we explore eating for your dosha...",
-        "image_path": "img/models/3.jpg",
+        "image_path": "img/3.jpg",
         "link": "Ayurvedic_Diet_Blog_Page"
     },
     {
         "title": "Integrating Ayurveda with Modern Medical Treatments 🌟",
         "description": "Ayurveda and modern medicine have unique strengths that can complement each other for holistic health...",
-        "image_path": "img/models/4.jpg",
+        "image_path": "img/4.jpg",
         "link": "Holistic_Healing_Blog_Page"
     },
     {
         "title": "Seasonal Ayurvedic Practices: Staying Healthy All Year 🌿🕰️",
         "description": "Ayurveda emphasizes living in harmony with the natural rhythms of the seasons to maintain balance...",
-        "image_path": "img/models/5.jpg",
+        "image_path": "img/5.jpg",
         "link": "Seasonal_Ayurvedic_Practices_Blog_Page"
     },
     {
         "title": "Managing Stress with Ayurveda 🌿🧘‍♀️",
         "description": "Meditation is a powerful Ayurvedic practice that calms the mind and reduces the effects of stress...",
-        "image_path": "img/models/6.jpg",
+        "image_path": "img/6.jpg",
         "link": "Managing_Stress_with_Ayurveda_Blog_Page"
     },
     {
         "title": "Detoxing the Ayurvedic Way: Panchakarma Explained 🌿✨",
         "description": "Panchakarma, which translates to 'five actions' in Sanskrit, is the ultimate Ayurvedic detoxification...",
-        "image_path": "img/models/7.jpg",
+        "image_path": "img/7.jpg",
         "link": "Detoxing_the_Ayurvedic_Way_Blog_Page"
     },
     {
         "title": "Ayurvedic Beauty Secrets: Natural Ways to Glow 🌿✨",
         "description": "Abhyanga is a daily self-massage practice using warm oil to nourish the skin and calm the nervous system...",
-        "image_path": "img/models/8.jpg",
+        "image_path": "img/8.jpg",
         "link": "Ayurvedic_Beauty_Secrets_Blog_Page"
     }
 ]
